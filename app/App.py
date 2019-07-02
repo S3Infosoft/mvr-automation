eedeff00516fbb898f5c9db43511d816b5ea3ff2
gp499 <46984957+gp499@users.noreply.github.com>
2019-07-02 13:06:33 +0530

    Update App.py

--- app/App.py
+++ app/App.py
@@ -1,29 +1,22 @@
 from flask import Flask
 from flask import json
 from flask import request
-
-from Common import run
 from bookingdotcom import BookingDotComImpl
 from goibibo import GoibiboImpl
 from mmt import MMTImpl
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
-
-@app.route('/automation', methods = ['POST'])
-def automation():
-    rdata = request.json
-    run(rdata['ota_name'],rdata['month'],rdata['year'], rdata['cin'], rdata['cout'])
-    return "JSON Message: " + json.dumps(request.json)
 
 
 @app.route('/automation/v1/booking', methods = ['POST'])
 def automation_booking():
     rdata = request.json
-    target = BookingDotComImpl(rdata['property_id'], rdata['checkin_date'], rdata['checkout_date'])
+    target = BookingDotComImpl(rdata['search_text'], rdata['checkin_date'], rdata['checkout_date'])
     result = target.run()
     return "Invoked Booking.com API v1: " + json.dumps(result)
 
@@ -31,7 +24,7 @@ def automation_booking():
 @app.route('/automation/v1/mmt', methods = ['POST'])
 def automation_mmt():
     rdata = request.json
-    target = MMTImpl(rdata['place'], rdata['checkin_date'], rdata['checkout_date'])
+    target = MMTImpl(rdata['search_text'], rdata['checkin_date'], rdata['checkout_date'])
     result = target.run()
     return "Invoked MMT API v1: " + json.dumps(result)
 
@@ -39,7 +32,7 @@ def automation_mmt():
 @app.route('/automation/v1/goibibo', methods = ['POST'])
 def automation_goibibo():
     rdata = request.json
-    target = GoibiboImpl(rdata['place'], rdata['checkin_date'], rdata['checkout_date'])
+    target = GoibiboImpl(rdata['search_text'], rdata['checkin_date'], rdata['checkout_date'])
     result = target.run()
     return "Invoked Goibibo API v1: " + json.dumps(result)
 
@@ -54,6 +47,7 @@ def automation_yatra():
 def automation_tg():
     rdata = request.json
     return "Invoked Travelguru API v1: " + json.dumps(rdata)
+
 
 @app.route('/automation/v1/airbnb', methods = ['POST'])
 def automation_airbnb():
